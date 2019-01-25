92c076a791d711433c10bc6bdd701cbd8144d71d
Christopher-at-404586615943 <cfox1290@gmail.com>
2019-01-25 14:35:50 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/acme/autocert"
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	g errgroup.Group
+)
+
+func main() {
+
+	server01 := &http.Server{
+		Addr:         ":443",
+		Handler:      frontend(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	g.Go(func() error {
+		return server01.ListenAndServe()
+	})
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func frontend() http.Handler {
+	app := gin.New()
+	app.Use(gin.Recovery())
+	app.Use(static.Serve("/", static.LocalFile("./pkg/build", false)))
+	//allows all connection requests
+	app.Use(cors.Default())
+
+	m := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("foxchrisrealthe.com"),
+		Cache:      autocert.DirCache("/var/www/.cache"),
+	}
+	autotls.RunWithManager(app, &m)
+
+	return app
+}
---
+++ pkg/build/precache-manifest.b593ced9cf6edc49f3200fe831d3463a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ea304f606155b56530f",
+    "url": "/static/js/main.1ea304f6.chunk.js"
+  },
+  {
+    "revision": "dd672678919165cc13f6",
+    "url": "/static/js/1.dd672678.chunk.js"
+  },
+  {
+    "revision": "1ea304f606155b56530f",
+    "url": "/static/css/main.7c96e198.chunk.css"
+  },
+  {
+    "revision": "3a87f3c1a72daf9aa6426c7d21532425",
+    "url": "/index.html"
+  }
+];
