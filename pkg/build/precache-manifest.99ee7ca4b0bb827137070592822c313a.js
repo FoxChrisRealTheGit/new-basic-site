cbd286cdf455986e5b8f45681d43a02d80616767
unknown <christopher.fox.coder@gmail.com>
2019-05-30 08:16:06 -0600

    some new things

---
+++ pkg/build/precache-manifest.99ee7ca4b0bb827137070592822c313a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b9235a71e17ffcc6a9fbdab8a3713c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b5860c9ec9bd313517",
+    "url": "/static/css/main.32f4597d.chunk.css"
+  },
+  {
+    "revision": "af22ad34bb95b27d3337",
+    "url": "/static/js/2.b36a01d2.chunk.js"
+  },
+  {
+    "revision": "90b5860c9ec9bd313517",
+    "url": "/static/js/main.111a813d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
